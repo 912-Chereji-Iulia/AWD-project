46393698165ba9d013cbc933d233e6ff3fbc9c68
Chereji Iulia <72073380+912-Chereji-Iulia@users.noreply.github.com>
2025-01-11 16:55:16 +0200

    first commit

---
+++ dance-project/src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent {
+  title = 'Welcome to the World of Dance';
+  introText = 'Dance is a beautiful form of art, expression, and culture. Let’s explore it together!';
+  images = [
+    'assets/dans1.jpg',
+    'assets/IMG_3782.jpg',
+    'assets/IMG_3784.jpg',
+    'assets/IMG_3786.JPG',
+    'assets/IMG_3789.jpg',
+    'assets/IMG_3790.jpg',
+  ];
+  typesOfDance = [
+    'Ballet',
+    'Jazz',
+    'Hip Hop',
+    'Contemporary',
+    'Ballroom'
+  ];
+  historyText = 'Dance has existed since the dawn of human culture. It is both a form of social interaction and a way to express emotions and ideas through movement.';
+  funFact = 'Did you know? Dance is one of the oldest human activities, predating written history.';
+  activeIndex = 0; // Track the current image
+  
+  // Move to the next image
+  nextImage(): void {
+    if (this.activeIndex < this.images.length - 1) {
+      this.activeIndex++;
+    } else {
+      this.activeIndex = 0; // Loop back to the first image
+    }
+  }
+
+  // Move to the previous image
+  prevImage(): void {
+    if (this.activeIndex > 0) {
+      this.activeIndex--;
+    } else {
+      this.activeIndex = this.images.length - 1; // Loop back to the last image
+    }
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
---
+++ dance-project/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';  // <-- Import AppRoutingModule
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
